43c815d747b9a5cfcb498f7b42612bf1fdde9383
MaerS <91330403+KamilKonopski@users.noreply.github.com>
2022-11-04 02:08:50 +0100

    Update MoonPage.vue

--- src/views/MoonPage.vue
+++ src/views/MoonPage.vue
@@ -30,11 +30,12 @@ export default {
 <style lang="scss" scoped>
 .moon-element {
   & h3 {
-    text-align: center;
-    margin-top: 5rem;
-    font-size: 5rem;
-    text-transform: uppercase;
     color: #4d4d4d;
+    font-size: 5rem;
+    margin-top: 5rem;
+    text-align: center;
+    text-transform: uppercase;
+    
   }
 }
 </style>
